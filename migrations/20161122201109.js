920dd69af1720fe73cc8904b317b6fda1fe71bc1
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2016-11-23 11:29:25 +0530

    wrong column in migration :unamused:

--- migrations/20161122201109.js
+++ migrations/20161122201109.js
@@ -1,7 +1,7 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('users', table => {
         table.increments();
-        table.string('name');
+        table.string('email');
         table.timestamps();
     });
 };
